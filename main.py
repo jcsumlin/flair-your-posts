39462804937e883d16c5a9619aa76184fba4f74e
J_C___ <jcsumlin@gmail.com>
2018-05-20 22:22:05 -0400

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import praw
-import io
 import os
-import re
 import configparser
-from datetime import timedelta
-from time import time
+from datetime import timedelta, datetime
 config = configparser.ConfigParser()
-config.read('auth.ini') #All my usernames and passwords for the api
+config.read('auth.ini')  # All my usernames and passwords for the api
 
 reddit = praw.Reddit(client_id=config.get('auth', 'reddit_client_id'),
                      client_secret=config.get('auth', 'reddit_client_secret'),
@@ -18,31 +15,38 @@ print("Posting as: ", reddit.user.me())
 SUBREDDIT = config.get('auth', 'reddit_subreddit')
 LIMIT = int(config.get('auth', 'reddit_limit'))
 
-#If the meep_replys text file dosn't exist, create it and initilize the enpty list.
+# If the meep_replys text file dosn't exist, create it and initilize the enpty list.
 if not os.path.isfile("posts_replied.txt"):
     posts_replied = []
-#If the file does exist import the contents into a list
+# If the file does exist import the contents into a list
 else:
     with open("posts_replied.txt", "r") as f:
        posts_replied = f.read()
        posts_replied = posts_replied.split("\n")
        posts_replied = list(filter(None, posts_replied))
 
+
 def reply_bot(debug=False):
     subreddit = reddit.subreddit(SUBREDDIT)
     new_posts = subreddit.new(limit=LIMIT)
     for submission in new_posts:
-		if (submission.link_flair_text is None) and (submission.created_utc - datetime.utcnow() == timedelta(minutes=1):
-			submission.reply("Hey there! Don't forget to flair your posts c: don't want to anger the mods ;)" + bot_message)
+        if (submission.link_flair_text is None) and (datetime.utcnow() - datetime.fromtimestamp(submission.created_utc) >= timedelta(minutes=10)) and submission.id not in posts_replied:
+            submission.reply("Hey there u/" + submission.author.name + "! Don't forget to flair your posts c: don't want to anger the mods ;)" + bot_message)
+            if debug is True:
+                print('Submission replied to:', submission.id)
+            posts_replied.append(submission.id)
+            update_files(posts_replied)
 
 
 def update_files(posts_replied):
     with open("posts_replied.txt", "w") as f:
         for x in posts_replied:
             f.write(x + "\n")
+
+
 try:
     while True:
-        reply_bot()
+        reply_bot(True)
 except KeyboardInterrupt:
     update_files(posts_replied)
-    print('Interrupted')
+    print('Interrupted, files updated')
