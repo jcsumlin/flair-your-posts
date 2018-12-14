17243e97d1524372e5f9a664b53a41001774d941
J_C___ <jcsumlin@gmail.com>
2018-12-14 00:32:40 -0600

    updated logging and push notifications

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import configparser
-import logging
+import logger
 import os
 import time
 from datetime import timedelta, datetime
+from loguru import logger
 
-import coloredlogs
 import praw
-from pushbullet import Pushbullet
+from pushover import init, Client
 
-coloredlogs.install()
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logger.add(f"file_{datetime.now()}.log", rotation="12:00")
+
+
 config = configparser.ConfigParser()
 config.read('auth.ini')  # All my usernames and passwords for the api
-pb = Pushbullet(str(config.get('auth', 'pb_key')))
+init(os.environ['pushover_key'])
+# init(config.get('auth', 'pushover_key'))
+pushover = Client(os.environ['pushover_client_key'])
+# pushover = Client(config.get('auth', 'pushover_client_key'))
 
 reddit = praw.Reddit(client_id=config.get('auth', 'reddit_client_id'),
                      client_secret=config.get('auth', 'reddit_client_secret'),
@@ -43,44 +47,45 @@ else:
         warning_posts = list(filter(None, warning_posts))
 
 rule_1 = "\r\r>**All posts must be assigned a flair**. If you don't flair your post, it will be removed. You can flair on mobile by posting it in your title, such as [Theory] Toffee is Star's brother. [Here is a list of flair options](https://www.reddit.com/r/StarVStheForcesofEvil/wiki/flairs). If your post is removed by the flair bot, **do NOT repost it**: [message the mods](https://www.reddit.com/message/compose?to=%2Fr%2FStarVStheForcesofEvil&subject=&message=) and we'll approve it. Unflaired posts are subject to removal without warning at any time."
+
+@logger.catch
 def reply_bot():
     subreddit = reddit.subreddit(SUBREDDIT)
     new_posts = subreddit.new(limit=LIMIT)
     for submission in new_posts:
         if (submission.link_flair_text is None) and (datetime.utcnow() - datetime.fromtimestamp(submission.created_utc) >= timedelta(minutes=10)) and submission.id not in warning_posts:
             submission.reply("Hey there u/" + submission.author.name + ", Don't forget to flair your posts!" + bot_message)
-            logging.info('Submission warned:' + str(submission.id))
+            logger.info('Submission warned:' + str(submission.id))
             warning_posts.append(submission.id)
             update_files(warning_posts)
         if (submission.link_flair_text is None) and (datetime.utcnow() - datetime.fromtimestamp(submission.created_utc) >= timedelta(minutes=15)) and submission.id not in posts_replied:
             submission.reply("Hey there u/" + submission.author.name + "! Your post has been removed in accordance with rule #1 of this subreddit." + rule_1 + bot_message)
             submission.mod.remove()
-            logging.info('Submission removed:' + str(submission.id))
+            logger.info('Submission removed:' + str(submission.id))
             posts_replied.append(submission.id)
             update_files(posts_replied)
 
-
+@logger.catch
 def update_files(list):
     with open("posts_replied.txt", "w") as f:
         for x in list:
             f.write(x + "\n")
-    logging.info("Progress File Successfully Updated!")
+    logger.info("Progress File Successfully Updated!")
 
 if __name__ == "__main__":
     try:
-        logging.info("------Starting: Flair Your Post Bot------")
-        logging.info("Posting as: %s" % reddit.user.me())
+        logger.info("------Starting: Flair Your Post Bot------")
+        logger.info("Posting as: %s" % reddit.user.me())
         while True:
             try:
                 reply_bot()
             except Exception as e:
-                logging.critical("Uncaught error: %s" % e)
+                logger.critical("Uncaught error: %s" % e)
                 time.sleep(30)
                 pass
     except KeyboardInterrupt:
         update_files(posts_replied)
-        logging.warning('Script interrupted')
+        logger.warning('Script interrupted')
     finally:
         update_files(posts_replied)
-        push = pb.push_note("SCRIPT Down", "J_C___ Flair-your-posts Script is Down!")
-
+        pushover.send_message("J_C___ Flair-your-posts Script is Down!", title="SCRIPT Down")
